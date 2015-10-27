8cb990b73012d8cf771898ec8456eaa13e440795
cnx <bobchennan@gmail.com>
2015-10-28 00:08:29 +0800

    Update deploy.js

--- client/deploy.js
+++ client/deploy.js
@@ -1,16 +1,62 @@
 var React = require('react')
-var exec = require('child_process').exec;
-
+var divStyle = {
+    whiteSpace: 'nowrap'
+  };
 var Deploy = React.createClass({
-  render: function () {
-    exec("~/workspace/deploy.sh", function (error, stdout, stderr) {
-        return <div className="deploy">
-        <h1>Output</h1>
-        <p><strong>{stdout}</strong></p>
-	    	<h1>Error</h1>
-	    	<p><strong>{stderr}</strong></p>
-        </div>
+  getInitialState: function() {
+    return {
+      stdout: '',
+      stderr: ''
+    };
+  },
+  handleSubmit: function(e) {
+    console.log(this.refs.commit.state.value);
+    e.preventDefault();
+	this.setState({
+      stdout: 'loading',
+      stderr: 'loading'
     });
+    $.post("/admin/deploy", {commit:this.refs.commit.state.value}, function(result) {
+	  if (this.isMounted()) {
+	    var lines = result.stdout.trim();
+	    var br = lines.split('\n').map(function(line) {
+            if(line==="")
+			    return "None";
+		    else
+			    return (<span>{line}<br/></span>);
+        });
+	    var lines2 = this.state.stderr.trim();
+	    var br2 = lines2.split('\n').map(function(line) {
+            if(line==="")
+			    return "None";
+		    else
+			    return (<span>{line}<br/></span>);
+        });
+		if(!!br2){
+			br2="None";
+		}
+        this.setState({
+          stdout: br,
+          stderr: br2
+        });
+      }
+    }.bind(this));
+  },
+  render: function () {
+    
+    return (
+	    <div className="deploy" style={divStyle}>
+	    <form onSubmit={this.handleSubmit}>
+		  <input type="text" placeholder="commit log" ref="commit" />
+		  <input type="submit" value="Deploy" />
+		</form>
+        <h1>Output</h1>
+        <p><strong>{this.state.stdout}</strong></p>
+	    <h1>Error</h1>
+	    <p><strong>{this.state.stderr}</strong></p>
+        </div>
+		)
+    ;
   }
 })
 
