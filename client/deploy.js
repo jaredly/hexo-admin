732cc2f08b84ce869c883ad967b6c37638f5b24f
cnx <bobchennan@gmail.com>
2015-10-27 17:00:48 +0800

    Update deploy.js

--- client/deploy.js
+++ client/deploy.js
@@ -1,8 +1,16 @@
 var React = require('react')
+var exec = require('child_process').exec;
 
 var Deploy = React.createClass({
   render: function () {
-    return process.cwd()
+    exec("~/workspace/deploy.sh", function (error, stdout, stderr) {
+        return <div className="deploy">
+        <h1>Output</h1>
+        <p><strong>{stdout}</strong></p>
+	    	<h1>Error</h1>
+	    	<p><strong>{stderr}</strong></p>
+        </div>
+    });
   }
 })
 
