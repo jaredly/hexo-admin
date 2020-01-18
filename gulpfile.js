e669c4e3eb8dbb650cdf1e3d0526fabff6c4d981
Robert Pirtle <astropirtle@gmail.com>
2020-01-17 16:24:30 -0800

    upgrade many packages, including gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,11 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
-// var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var reactify = require('reactify');
 var rename = require('gulp-rename');
 
-gulp.task('demo', function () {
+function demo() {
   var b = browserify({
     entries: './docs/demo/run.js',
     debug: true,
@@ -21,9 +19,9 @@ gulp.task('demo', function () {
     .pipe(buffer())
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('./docs/demo/admin/'));
-});
+};
 
-gulp.task('javascript', function () {
+function javascript() {
   var b = browserify({
     entries: './client/run.js',
     debug: true,
@@ -33,31 +31,27 @@ gulp.task('javascript', function () {
   return b.bundle()
     .pipe(source('./client/run.js'))
     .pipe(buffer())
-    // .pipe(sourcemaps.init({loadMaps: true}))
-    // Add transformation tasks to the pipeline here.
-    // .pipe(uglify())
-    // .on('error', gutil.log)
-    // .pipe(sourcemaps.write('./'))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('./www/'));
-});
+};
 
-var less = require('gulp-less');
+var gulpLess = require('gulp-less');
 
-gulp.task('less', function () {
+function less() {
   return gulp.src('./client/less/index.less')
-    .pipe(less({
-      // paths: [path.join(__dirname, 
-    }))
+    .pipe(gulpLess({}))
     .pipe(rename('bundle.css'))
     .pipe(gulp.dest('./www'))
-});
+};
 
-gulp.task('build', ['less', 'javascript']);
+async function build() { return gulp.parallel(less, javascript); }
 
-gulp.task('watch', function () {
-  gulp.watch('client/**/*.js', ['javascript']);
-  gulp.watch('client/**/*.less', ['less']);
-});
+function watch () {
+  gulp.watch('client/**/*.js', gulp.series(javascript));
+  gulp.watch('client/**/*.less', gulp.series(less));
+};
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('demo', demo);
+gulp.task('build', build);
+gulp.task('watch', watch);
+gulp.task('default', gulp.series('build'));
